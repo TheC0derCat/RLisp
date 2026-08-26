fdbcc5a3fa37497e555abf375abc91933801caec
Silly Cat <CatC0der@proton.me>
2026-08-26 01:39:44 -0700

    PARSER ADDEDgit add --all! :DDDDD

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use std::env;
 use std::fs;
 
-#[derive(Debug, PartialEq)]
+#[derive(Debug, PartialEq, Clone)]
 enum Operator {
     Add,
     Sub,
@@ -9,7 +9,7 @@ enum Operator {
     Div,
     Mod,
 }
-#[derive(Debug, PartialEq)]
+#[derive(Debug, PartialEq, Clone)]
 enum Token {
     OpeningParen,
     ClosingParen,
@@ -22,6 +22,7 @@ enum Token {
 struct Lexer {
     buf: Vec<char>,
     ptr: usize,
+    last: Token,
 }
 impl Lexer {
     fn getch(&mut self) -> char {
@@ -38,7 +39,7 @@ impl Lexer {
             return Token::FileEnd;
         }
         let mut ch: char = self.getch();
-        if ch.is_alphanumeric() {
+        self.last = if ch.is_alphanumeric() {
             let mut tempbuf: String = String::new();
             while ch.is_alphanumeric() {
                 tempbuf.push(ch);
@@ -70,7 +71,22 @@ impl Lexer {
                 },
                 _ => self.next_token(),
             }
-        }
+        };
+        return self.last.clone();
+    }
+}
+fn put_file_tokens(file_path: String) {
+    let contents = fs::read_to_string(file_path)
+        .expect("File does not exist");
+    let mut lexer: Lexer = Lexer{
+        buf: contents.chars().collect(),
+        ptr: 0,
+        last: Token::FileEnd,
+    };
+    let mut tok: Token = lexer.next_token();
+    while tok != Token::FileEnd {
+        println!("{:?}", tok);
+        tok = lexer.next_token();
     }
 }
 #[derive(Debug, PartialEq)]
@@ -82,19 +98,47 @@ enum Value {
 enum ASTNode {
     Litteral(Value),
     Operator(Operator, Vec<ASTNode>),
+    End,
+}
+fn parser(lexer: &mut Lexer) -> ASTNode {
+    let tok: Token = lexer.next_token();
+    match tok {
+        Token::OpeningParen => {
+            let tok: Token = lexer.next_token();
+            if let Token::Operator(Operator) = tok {
+                let mut branchs: Vec<ASTNode> = Vec::new();
+                while lexer.last != Token::ClosingParen {
+                    let new_node: ASTNode = parser(lexer);
+                    if new_node == ASTNode::End {
+                        break;
+                    }
+                    else {
+                        branchs.push(new_node);
+                    }
+                }
+                ASTNode::Operator(Operator, branchs)
+            }
+            else {
+                panic!("unexpected token {:?}", tok);
+            }
+        },
+        Token::ClosingParen => ASTNode::End,
+        Token::Num(i) => ASTNode::Litteral(Value::Int(i)),
+        Token::Str(i) => ASTNode::Litteral(Value::Str(i)),
+        _ => panic!("unexpected token {:?}", tok),
+    }
 }
 fn main() {
     let args: Vec<String> = env::args().collect();
     let file_path: String = args[1].clone();
+    // put_file_tokens(file_path);
     let contents = fs::read_to_string(file_path)
         .expect("File does not exist");
     let mut lexer: Lexer = Lexer{
         buf: contents.chars().collect(),
         ptr: 0,
+        last: Token::FileEnd,
     };
-    let mut tok: Token = lexer.next_token();
-    while tok != Token::FileEnd {
-        println!("{:?}", tok);
-        tok = lexer.next_token();
-    }
+    let astnode: ASTNode = parser(&mut lexer);
+    println!("{:?}", astnode);
 }
